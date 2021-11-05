e8e810a64b61b08d32387cbc8f680205f5530522
eaguiler <elisha.aguilera@wsu.edu>
2021-11-04 22:03:10 -0700

    Testing doc-strings

--- src/Test.py
+++ src/Test.py
@@ -22,16 +22,20 @@ class TestManifest(unittest.TestCase):
     sample_manifest_request: str = '{"requests:["Sample-v0.0.1"]"}'
 
     def test_convert(self):
+        """Testing conversion between JSON file and Dictionary"""
         self.assertEqual(self.sample_manifest_parse, Manifest.file_parse(self, self.sample_manifest_path), "Files should match.")
     
     def test_class_convert(self):
+        """Testing class conversion between JSON file and class dictionary"""
         sample_manifest = Manifest(self.sample_manifest_path)
         self.assertEqual(self.sample_manifest_parse, sample_manifest.manifest, "Class attribute should match sample")
     
     def test_class_dependencies(self):
+        """Testing dependency list results"""
         self.assertEqual(self.sample_manifest_dependencies, Manifest(self.sample_manifest_path).dependencies(), "Dependency list should match")
     
     def test_class_request(self):
+        """Testing request and return results"""
         sample_manifest = Manifest(self.sample_manifest_path)
         self.assertRaises(Exception,make_request(sample_manifest.registry, sample_manifest.dependencies))
 
