42586a8d83ec06dbdeb164ddfea60f5343a10f55
EJAguilera <elisha.aguilera@wsu.edu>
2021-11-10 03:17:57 +0000

    Altered sample manifest file, and then added init

--- src/command_line/commands.py
+++ src/command_line/commands.py
@@ -1,9 +1,19 @@
 import click
 
 from src.file_config import File_config
+from src.manifest.manifest import Manifest
 
 @click.group()
 def cli():
+    pass
+
+@cli.command()
+@click.pass_context
+def init(ctx):
+    manifest = Manifest('sample_manifest.json')
+    for dependency in manifest.dependencies():
+        click.echo(f'Loading {dependency} package...')
+        ctx.invoke(config, name=dependency)
     pass
 
 @cli.command()
